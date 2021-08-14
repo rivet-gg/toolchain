0fbd03e1bfac75a30402106710c6d649b1699bdd
Nathan Flurry <developer@nathanflurry.com>
2021-08-14 22:10:53 +0000

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+    # Include most recent Rust builds
+    moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+    pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+    pkgs.mkShell {
+        name = "rivetctl";
+        buildInputs = with pkgs; [
+            cacert
+
+            (pkgs.latest.rustChannels.stable.rust.override {
+                targets = [ "wasm32-unknown-unknown" ];
+            })
+            pkg-config
+            openssl
+        ];
+    }
+
