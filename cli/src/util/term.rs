3a7fd6209aa65f75823552f3882e124b2309e865
Nathan Flurry <developer@nathanflurry.com>
2022-08-10 20:55:06 -0700

    Impl wrapping in tables

--- cli/src/util/term.rs
+++ cli/src/util/term.rs
@@ -1,8 +1,12 @@
 use tabled::{Table, Tabled};
 
-pub fn table<T>(iter: impl IntoIterator<Item = T>) 
+pub fn table<T>(iter: impl IntoIterator<Item = T>)
 where
 	T: Tabled,
 {
-	println!("{}", Table::new(iter).with(tabled::Style::rounded()));
+	let mut table = Table::new(iter).with(tabled::Style::rounded());
+	if let Some((w, _)) = term_size::dimensions() {
+		table = table.with(tabled::Width::wrap(w));
+	}
+	println!("{}", table);
 }
