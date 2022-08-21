59df826e82e32fc1d9879b36734f6b68ebb62a1d
Nathan Flurry <developer@nathanflurry.com>
2022-08-20 17:27:47 -0700

    Try adding more libs

--- bin/pre-build.sh
+++ bin/pre-build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -euf -o pipefail
 
-echo "Installing pkgconfig & openssl-dev"
 apk update
-apk add --no-cache pkgconfig openssl-dev
+apk add --no-cache pkgconfig openssl-dev gcc musl-dev
 
