348dae1ed2046ca27429b3ffe6a2803dd400a908
Nathan Flurry <developer@nathanflurry.com>
2022-08-20 18:42:51 -0700

    Update install script

--- install/unix.sh
+++ install/unix.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -euf -o pipefail
 
+VERSION=0.0.16
+
 cd /tmp
-curl --proto '=https' --tlsv1.2 -sSfL https://github.com/rivet-gg/cli/releases/download/0.0.16/cli_0.0.16_x86_64-unknown-linux-musl.tar.gz -o rivet.tar.gz
+curl --proto '=https' --tlsv1.2 -sSfL "https://github.com/rivet-gg/cli/releases/download/${VERSION}/cli_${VERSION}_x86_64-unknown-linux-musl.tar.gz" -o rivet.tar.gz
 tar xzf rivet.tar.gz
 mv ./rivet /usr/local/bin/rivet
 
