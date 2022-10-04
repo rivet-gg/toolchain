1a8232081a667e9b56b814142eca36a340fdedbe
Nathan Flurry <developer@nathanflurry.com>
2022-10-04 17:09:58 +0000

    Update install script to find the last version with a matching build

--- install/unix.sh
+++ install/unix.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 set -eu
 
+# Find asset suffix
+if [ "$(uname)" = "Darwin" ]; then
+	echo
+	echo "> Detected macOS"
+
+	CLI_ASSET_SUFFIX="_x86_64-apple-darwin.zip"
+elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
+	echo
+	echo "> Detected Linux"
+
+	CLI_ASSET_SUFFIX="_x86_64-unknown-linux-musl.tar.gz"
+else
+	echo "Unable to determine platform" 1>&2
+	exit 1
+fi
+
+# Find CLI version
 set +u
 if [ -z "$RIVET_CLI_VERSION" ]; then
 	echo
 	echo "> Fetching latest release version"
-	RIVET_CLI_VERSION="$(curl -fsSL https://api.github.com/repos/rivet-gg/cli/releases/latest | jq -r '.name')"
+	RIVET_CLI_VERSION="$( \
+		curl -fsSL https://api.github.com/repos/rivet-gg/cli/releases
+		| jq -re \
+			--arg cli_asset_suffix "$CLI_ASSET_SUFFIX" \
+			'[.[] | select(.assets[] | select(.name | endswith($cli_asset_suffix)))] | first | .name' \
+	)"
 fi
 set -u
 
@@ -15,9 +37,6 @@ echo "> Installing Rivet CLI @ $RIVET_CLI_VERSION"
 cd /tmp
 
 if [ "$(uname)" = "Darwin" ]; then
-	echo
-	echo "> Detected macOS"
-
 	echo
 	URL="https://github.com/rivet-gg/cli/releases/download/${RIVET_CLI_VERSION}/cli_${RIVET_CLI_VERSION}_x86_64-apple-darwin.zip"
 	echo "> Downloading $URL"
@@ -32,9 +51,6 @@ if [ "$(uname)" = "Darwin" ]; then
 	sudo mv ./rivet /usr/local/bin/rivet
 elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
 	echo
-	echo "> Detected Linux"
-
-	echo
 	URL="https://github.com/rivet-gg/cli/releases/download/${RIVET_CLI_VERSION}/cli_${RIVET_CLI_VERSION}_x86_64-unknown-linux-musl.tar.gz"
 	echo "> Downloading $URL"
 	curl -fsSL "$URL" -o rivet.tar.gz
@@ -47,7 +63,6 @@ elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
 	echo "> Installing rivet"
 	sudo mv ./rivet /usr/local/bin/rivet
 else
-	echo "Unable to determine platform" 1>&2
 	exit 1
 fi
 
