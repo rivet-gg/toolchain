0a84e988d1fc694799f01bf2aaea9aef4651cce2
Nathan Flurry <developer@nathanflurry.com>
2022-10-05 15:25:15 +0000

    Add rudimentary config parsing test

---
+++ cli/tests/version.rs
@@ -0,0 +1,22 @@
+use serde_json::json;
+
+#[tokio::test]
+async fn basic() {
+	std::env::set_current_dir(
+		std::env::current_dir()
+			.unwrap()
+			.join("tests")
+			.join("static")
+			.join("basic"),
+	)
+	.unwrap();
+	let _user_config = rivet::commands::version::read_user_config(
+		vec![
+			("cdn.site".into(), json!("xxxx")),
+			("matchmaker.docker.build".into(), json!("xxxx")),
+		],
+		Some("my-ns"),
+	)
+	.await
+	.unwrap();
+}
