801a7a7bdb30ff0adbee99255422ae843e98a4bf
Nathan Flurry <developer@nathanflurry.com>
2022-11-29 22:43:35 -0700

    Fix shell.nix on macOS

--- shell.nix
+++ shell.nix
@@ -1,18 +1,20 @@
 let
-    # Include most recent Rust builds
-    moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-    pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+	moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/80627b282705101e7b38e19ca6e8df105031b072.tar.gz);
+	pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
 in
-    pkgs.mkShell {
-        name = "rivet-cli";
-        buildInputs = with pkgs; [
-            cacert
+	pkgs.mkShell {
+		name = "rivet-cli";
+		buildInputs = with pkgs; [
+			cacert
 
-            (pkgs.latest.rustChannels.stable.rust.override {
-                targets = [ "wasm32-unknown-unknown" ];
-            })
-            pkg-config
-            openssl
-        ];
-    }
+			pkgs.latest.rustChannels.stable.rust
+			pkg-config
+			openssl
+			libiconv
+		] ++ (
+			pkgs.lib.optionals stdenv.isDarwin [
+				darwin.apple_sdk.frameworks.Security
+			]
+		);
+	}
 
