ff9fb52d2a19aff2516ac1bfeb437bffdc8f0bb4
Nathan Flurry <developer@nathanflurry.com>
2023-03-14 21:12:37 +0000

    Update copy libs script to inject deny_unknown_fields to serde structs

--- bin/copy-libs.sh
+++ bin/copy-libs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Copy libraries
 rm -rf lib/rivet-cloud/ lib/rivet-api/
 cp -r ../backend/gen/svc/api-cloud/smithy/rust/ lib/rivet-cloud/
 cp -r ../backend/gen/openapi/internal/rust/ lib/rivet-api/
+
+# Modify libraries
+find lib/rivet-api -name "*.rs" -exec sed -i 's/\(#\[derive.*Deserialize.*\]\)/\1\n#[serde(deny_unknown_fields)]/g' {} \;
 
