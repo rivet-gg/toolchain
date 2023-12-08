7e6771238a0919ad470803e230dba55231e7a4db
Nathan Flurry <git@nathanflurry.com>
2023-12-08 08:06:45 +0000

    impl new date format

--- cli/src/commands/version.rs
+++ cli/src/commands/version.rs
@@ -168,25 +168,25 @@ pub struct DeployOpts {
 	/// Deprecated.
 	///
 	/// The build tag to upload
-	#[clap(long)]
+	#[clap(hide = true, long)]
 	build_tag: Option<String>,
 
 	/// Deprecated.
 	///
 	/// The name to assign to the build
-	#[clap(long)]
+	#[clap(hide = true, long)]
 	build_name: Option<String>,
 
 	/// Deprecated.
 	///
 	/// The path to the site directory to upload
-	#[clap(long)]
+	#[clap(hide = true, long)]
 	site_path: Option<String>,
 
 	/// Deprecated.
 	///
 	/// The name of the site that will be created
-	#[clap(long)]
+	#[clap(hide = true, long)]
 	site_name: Option<String>,
 
 	#[clap(long, value_parser)]
@@ -198,6 +198,7 @@ impl DeployOpts {
 		// Parse overrides
 		let mut overrides = parse_config_override_args(&self.overrides)?;
 
+		// Build & push site & build before creating version
 		build_and_push_compat(
 			ctx,
 			&mut overrides,
@@ -209,6 +210,7 @@ impl DeployOpts {
 		)
 		.await?;
 
+		// Create version
 		let output = create(
 			ctx,
 			self.display_name.as_ref().map(String::as_str),
@@ -345,19 +347,20 @@ pub fn build_mock_config_dependencies(version: &mut models::CloudVersionConfig) 
 pub async fn build_config_dependencies(
 	ctx: &cli_core::Ctx,
 	version: &mut models::CloudVersionConfig,
+	display_name: &str,
 	format: Option<&struct_fmt::Format>,
 ) -> Result<()> {
 	// TODO: Do this for all possible docker endpoints
 
 	if let Some(matchmaker) = version.matchmaker.as_mut() {
 		if let Some(docker) = matchmaker.docker.as_mut() {
-			build_and_push_image(ctx, docker, format).await?;
+			build_and_push_image(ctx, display_name, docker, format).await?;
 		}
 
 		if let Some(game_modes) = matchmaker.game_modes.as_mut() {
 			for (_, game_mode) in game_modes.iter_mut() {
 				if let Some(docker) = game_mode.docker.as_mut() {
-					build_and_push_image(ctx, docker, format).await?;
+					build_and_push_image(ctx, display_name, docker, format).await?;
 				}
 			}
 		}
@@ -365,7 +368,7 @@ pub async fn build_config_dependencies(
 
 	// Build CDN
 	if let Some(cdn) = version.cdn.as_mut() {
-		build_and_push_site(ctx, cdn, format).await?;
+		build_and_push_site(ctx, display_name, cdn, format).await?;
 	}
 
 	Ok(())
@@ -373,6 +376,7 @@ pub async fn build_config_dependencies(
 
 pub async fn build_and_push_image(
 	ctx: &cli_core::Ctx,
+	display_name: &str,
 	docker: &mut Box<models::CloudVersionMatchmakerGameModeRuntimeDocker>,
 	format: Option<&struct_fmt::Format>,
 ) -> Result<()> {
@@ -382,7 +386,7 @@ pub async fn build_and_push_image(
 				ctx,
 				&image::BuildPushOpts {
 					dockerfile: dockerfile.clone(),
-					name: Some(gen::display_name_from_date()),
+					name: Some(display_name.to_string()),
 					format: format.cloned(),
 				},
 			)
@@ -393,7 +397,7 @@ pub async fn build_and_push_image(
 				ctx,
 				&image::PushOpts {
 					tag: docker_image.clone(),
-					name: Some(gen::display_name_from_date()),
+					name: Some(display_name.to_string()),
 					format: format.cloned(),
 				},
 			)
@@ -406,6 +410,7 @@ pub async fn build_and_push_image(
 }
 pub async fn build_and_push_site(
 	ctx: &cli_core::Ctx,
+	display_name: &str,
 	cdn: &mut Box<models::CloudVersionCdnConfig>,
 	format: Option<&struct_fmt::Format>,
 ) -> Result<()> {
@@ -417,7 +422,7 @@ pub async fn build_and_push_site(
 					&site::BuildPushOpts {
 						command: build_command.clone(),
 						path: build_output.clone(),
-						name: Some(gen::display_name_from_date()),
+						name: Some(display_name.to_string()),
 						format: format.cloned(),
 					},
 				)
@@ -428,7 +433,7 @@ pub async fn build_and_push_site(
 					ctx,
 					&site::PushOpts {
 						path: build_output.clone(),
-						name: Some(gen::display_name_from_date()),
+						name: Some(display_name.to_string()),
 						format: format.cloned(),
 					},
 				)
@@ -470,7 +475,6 @@ pub async fn create(
 	namespace_name_id: Option<&str>,
 	format: Option<&struct_fmt::Format>,
 ) -> Result<CreateOutput> {
-	let display_name = display_name.map_or_else(gen::display_name_from_date, |x| x.to_string());
 	// Fetch game data
 	let game_res =
 		cli_core::rivet_api::apis::cloud_games_games_api::cloud_games_games_get_game_by_id(
@@ -496,9 +500,16 @@ pub async fn create(
 		None
 	};
 
+	// Generate version name if needed
+	let display_name = if let Some(x) = &display_name {
+		x.to_string()
+	} else {
+		gen::version_display_name(&game_res.game)?
+	};
+
 	// Parse config
 	let mut rivet_config = read_config(overrides, namespace_name_id).await?;
-	build_config_dependencies(ctx, &mut rivet_config, format).await?;
+	build_config_dependencies(ctx, &mut rivet_config, &display_name, format).await?;
 
 	// Create game version
 	let version_res =
@@ -556,9 +567,10 @@ pub async fn create(
 	Ok(CreateOutput { version_id })
 }
 
-/// Backwards compatibility for site & Docker build pushing
+/// Backwards compatibility for site & Docker build pushing from the CLI flags.
 ///
-/// Developers should use the parameters inside the config itself instead
+/// Developers should override config properties instead. For example: `rivet deploy -o matchmaker.docker.image_id=xxxx -o
+/// cdn.path=xxxx`
 pub async fn build_and_push_compat(
 	ctx: &cli_core::Ctx,
 	overrides: &mut Vec<(String, serde_json::Value)>,
--- cli/src/util/gen.rs
+++ cli/src/util/gen.rs
@@ -1,5 +1,30 @@
-pub fn display_name_from_date() -> String {
-	// HACK: We can't allow multiple whitespace in a row, unsure why Chrono does
-	// this
-	format!("{}", chrono::Utc::now().format("%c")).replace("  ", " ")
+use anyhow::{Context, Result};
+use cli_core::rivet_api::models;
+
+pub fn version_display_name(game: &models::CloudGameFull) -> Result<String> {
+	// Generate date
+	//
+	// Use UTC in order to ensure that the month is consistent if a team is collaborating from
+	// multiple locations around the world with different time zones
+	let now = chrono::Utc::now();
+	let date_prefix = now.format("%Y.%m");
+
+	// Find the max index, if exists
+	let re = regex::Regex::new(&format!("{date_prefix} \\((\\d+)\\)")).unwrap();
+	let mut max_index = 0;
+	for version in &game.versions {
+		if let Some(captures) = re.captures(&version.display_name) {
+			let version_idx = captures
+				.get(1)
+				.context("regex capture")?
+				.as_str()
+				.parse::<i64>()?;
+			max_index = max_index.max(version_idx);
+		}
+	}
+
+	// Build new name
+	let version_name = format!("{date_prefix} ({index})", index = max_index + 1);
+
+	Ok(version_name)
 }
