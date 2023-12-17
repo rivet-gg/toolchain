cccf9f45cf3327289bad662a499f8dfe1345de3b
Nathan Flurry <git@nathanflurry.com>
2023-12-17 12:20:22 -0800

    Allow installing CLI in custom dir without using sudo

--- install/unix.sh
+++ install/unix.sh
@@ -25,20 +25,20 @@ if [ "$(printf '%s' "$UNAME" | cut -c 1-6)" = "Darwin" ]; then
 		url="https://github.com/jqlang/jq/releases/download/jq-1.7/jq-macos-amd64"
 	elif [ "$ARCH" = "arm64" ]; then
 		url="https://github.com/jqlang/jq/releases/download/jq-1.7/jq-macos-arm64"
-    else
-        echo "Unknown arch $ARCH" 1>&2
-        exit 1
+	else
+		echo "Unknown arch $ARCH" 1>&2
+		exit 1
 	fi
 	curl -fsSL "$url" -o ./jq
 	chmod +x ./jq
 
 	if [ "$ARCH" = "x86_64" ]; then
-        ASSET_NAME="rivet-cli-x86_64-apple-darwin"
+		ASSET_NAME="rivet-cli-x86_64-apple-darwin"
 	elif [ "$ARCH" = "arm64" ]; then
-        ASSET_NAME="rivet-cli-aarch64-apple-darwin"
-    else
-        echo "Unknown arch $ARCH" 1>&2
-        exit 1
+		ASSET_NAME="rivet-cli-aarch64-apple-darwin"
+	else
+		echo "Unknown arch $ARCH" 1>&2
+		exit 1
 	fi
 elif [ "$(printf '%s' "$UNAME" | cut -c 1-5)" = "Linux" ]; then
 	echo
@@ -66,19 +66,26 @@ set -u
 INSTALL_PATH="$BIN_DIR/rivet"
 
 if [ ! -d "$BIN_DIR" ]; then
+    # Find the base parent directory. We're using mkdir -p, which recursively creates directories, so we can't rely on `dirname`.
+    CHECK_DIR="$BIN_DIR"
+    while [ ! -d "$CHECK_DIR" ] && [ "$CHECK_DIR" != "/" ]; do
+        CHECK_DIR=$(dirname "$CHECK_DIR")
+    done
 
-	if command -v sudo; then
+    # Check if the directory is writable
+    if [ ! -w "$CHECK_DIR" ]; then
         echo
         echo "> Creating directory $BIN_DIR (requires sudo)"
-        sudo mkdir -p $BIN_DIR
-	else
+        sudo mkdir -p "$BIN_DIR"
+    else
         echo
         echo "> Creating directory $BIN_DIR"
-        mkdir -p $BIN_DIR
-	fi
+        mkdir -p "$BIN_DIR"
+    fi
+
 fi
 
-# Find CLI version
+# Find latest CLI version to download
 set +u
 if [ -z "$RIVET_CLI_VERSION" ]; then
 	echo
@@ -92,50 +99,32 @@ if [ -z "$RIVET_CLI_VERSION" ]; then
 fi
 set -u
 
+# Download CLI
+URL="https://github.com/rivet-gg/cli/releases/download/${RIVET_CLI_VERSION}/${FILE_NAME}"
 echo
-echo "> Installing Rivet CLI $RIVET_CLI_VERSION"
+echo "> Downloading $URL"
+curl -fsSL "$URL" -o rivet_cli.tar.xz
 
-if [ "$(printf '%s' "$UNAME" | cut -c 1-6)" = "Darwin" ]; then
+# Extract
+echo
+echo "> Extracting rivet.tar.xz"
+tar xJf rivet_cli.tar.xz
 
-	echo
-	URL="https://github.com/rivet-gg/cli/releases/download/${RIVET_CLI_VERSION}/${FILE_NAME}"
-	echo "> Downloading $URL"
-	curl -fsSL "$URL" -o rivet_cli.tar.xz
-
-	echo
-	echo "> Extracting rivet.zip"
-	tar xJf rivet_cli.tar.xz
-
-	echo
+# Move binary
+if [ ! -w "$BIN_DIR" ]; then
+    echo
     echo "> Installing rivet to $INSTALL_PATH (requires sudo)"
-	sudo mv "./${ASSET_NAME}/rivet-cli" "$INSTALL_PATH"
-elif [ "$(printf '%s' "$UNAME" | cut -c 1-5)" = "Linux" ]; then
-	echo
-	URL="https://github.com/rivet-gg/cli/releases/download/${RIVET_CLI_VERSION}/${FILE_NAME}"
-	echo "> Downloading $URL"
-	curl -fsSL "$URL" -o rivet_cli.tar.xz
-
-	echo
-	echo "> Extracting rivet.tar.gz"
-	tar xJf rivet_cli.tar.xz
-
-	if command -v sudo; then
-        echo
-        echo "> Installing rivet to $INSTALL_PATH (requires sudo)"
-		sudo mv "./${ASSET_NAME}/rivet-cli" "$INSTALL_PATH"
-	else
-        echo
-        echo "> Installing rivet to $INSTALL_PATH"
-		mv "./${ASSET_NAME}/rivet-cli" "$INSTALL_PATH"
-	fi
+    sudo mv "./${ASSET_NAME}/rivet-cli" "$INSTALL_PATH"
 else
-    echo 
-	exit 1
+    echo
+    echo "> Installing rivet to $INSTALL_PATH"
+    mv "./${ASSET_NAME}/rivet-cli" "$INSTALL_PATH"
 fi
 
+# Check if path may be incorrect
 case ":$PATH:" in
-    *:$BIN_DIR:*) ;;
-    *) echo "WARNING: $BIN_DIR is not in \$PATH" ;;
+	*:$BIN_DIR:*) ;;
+	*) echo "WARNING: $BIN_DIR is not in \$PATH" ;;
 esac
 
 echo
