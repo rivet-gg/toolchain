bc41c5d9f3c03c912c140d8398e6d7ec4506b0d8
Nathan Flurry <developer@nathanflurry.com>
2024-04-23 12:57:58 -0700

    testing vanilla curl upload

--- cli/src/util/upload.rs
+++ cli/src/util/upload.rs
@@ -113,111 +113,157 @@ pub async fn upload_file(
 		let file_len = file_meta.len();
 
 		let total_size = presigned_req.content_length as u64;
-		let is_multipart = total_size != file_len;
+		// let is_multipart = total_size != file_len;
+		//
+		// let msg = if is_multipart {
+		// 	format!("{path} {}", style("[CHUNK]").dim().blue(),)
+		// } else {
+		// 	path.clone()
+		// };
+		//
+		// // Add progress bar
+		// match &main_pb {
+		// 	term::EitherProgressBar::Single(_) => {}
+		// 	term::EitherProgressBar::Multi(mpb) => {
+		// 		pb.reset();
+		// 		pb.set_style(term::pb_style_file());
+		// 		pb.set_message(msg);
+		// 		pb.set_length(total_size);
+		//
+		// 		// Hack to fix weird bug with `MultiProgress` where it renders an empty progress bar and leaves
+		// 		// it there
+		// 		if !pb_added {
+		// 			pb.set_draw_target(indicatif::ProgressDrawTarget::stderr());
+		// 			mpb.add(pb.clone());
+		//
+		// 			pb_added = true;
+		//
+		// 			if !is_tty {
+		// 				eprintln!("Uploading {path} ({})", format_file_size(total_size)?);
+		// 			}
+		// 		}
+		// 	}
+		// }
+		//
+		// // Create a reader for the slice of the file we need to read
+		// file.seek(tokio::io::SeekFrom::Start(presigned_req.byte_offset as u64))
+		// 	.await?;
+		// let handle = file.take(presigned_req.content_length as u64);
+		//
+		// // Default buffer size is optimized for memory usage. Increase buffer for perf.
+		// let mut reader_stream = ReaderStream::with_capacity(handle, 1024 * 1024);
+		//
+		// let start = Instant::now();
+		//
+		// // Process the stream with upload progress
+		// let pb2 = pb.clone();
+		// let async_stream = async_stream::stream! {
+		// 	while let Some(chunk) = reader_stream.next().await {
+		// 		if let Ok(chunk) = &chunk {
+		// 			pb2.inc(chunk.len() as u64);
+		// 		}
+		//
+		// 		yield chunk;
+		// 	}
+		// };
+		//
+		// let body = reqwest::Body::wrap_stream(async_stream);
+		//
+		// // Upload file
+		// let mut req = reqwest_client
+		// 	.put(&presigned_req.url)
+		// 	.header("content-length", presigned_req.content_length);
+		// if let Some(content_type) = &content_type {
+		// 	req = req.header("content-type", content_type.to_string());
+		// }
+		// let res = req.body(body).send().await?;
+		// if res.status().is_success() {
+		// 	let upload_time = start.elapsed();
+		// 	println!("uplaod time {upload_time:?}");
+		// 	break 'upload (upload_time, total_size);
+		// } else {
+		// 	if attempts > 4 {
+		// 		let response_status = res.status();
+		// 		let response_text = res.text().await?;
+		// 		let text = format!(
+		// 			"failed to upload file: {}\n{:?}",
+		// 			response_status, response_text
+		// 		);
+		// 		bail!(&text);
+		// 	} else {
+		// 		attempts += 1;
+		//
+		// 		let status = res.status();
+		// 		let body_text = unwrap!(res.text().await);
+		//
+		// 		pb.set_style(term::pb_style_error());
+		// 		pb.set_message(format!(
+		// 			"{}{}{} {path} {retry_and_body}",
+		// 			style("[").bold().red(),
+		// 			style(status).bold().red(),
+		// 			style("]").bold().red(),
+		// 			path = style(&path).red(),
+		// 			retry_and_body =
+		// 				style(format!("will retry (attempt #{attempts}): {body_text:?}"))
+		// 					.dim()
+		// 					.red(),
+		// 		));
+		//
+		// 		if !is_tty {
+		// 			eprintln!(
+		// 				"Error uploading {path} [{status}] (attempt #{attempts}): {body_text:?}",
+		// 			);
+		// 		}
+		//
+		// 		tokio::time::sleep(Duration::from_secs(5)).await;
+		// 		continue 'upload;
+		// 	}
+		// }
 
-		let msg = if is_multipart {
-			format!("{path} {}", style("[CHUNK]").dim().blue(),)
-		} else {
-			path.clone()
-		};
+		// Split the file into parts and store them in /tmp
+		let name = uuid::Uuid::new_v4();
+		// let tmp_file_path = format!("/tmp/split_{name}");
+		let tmp_file_path = format!("/Users/nathan/Downloads/split_{name}");
+		let dd_command = format!(
+			"dd if={} of={} bs=1M skip={} count={} status=none",
+			file_path.as_ref().display(),
+			tmp_file_path,
+			presigned_req.byte_offset,
+			presigned_req.content_length
+		);
+		println!("DD command: {dd_command}");
+		let start = Instant::now();
+		tokio::process::Command::new("sh")
+			.arg("-c")
+			.arg(&dd_command)
+			.output()
+			.await
+			.unwrap();
+		let split_time = start.elapsed();
+		println!("split time {split_time:?}");
 
-		// Add progress bar
-		match &main_pb {
-			term::EitherProgressBar::Single(_) => {}
-			term::EitherProgressBar::Multi(mpb) => {
-				pb.reset();
-				pb.set_style(term::pb_style_file());
-				pb.set_message(msg);
-				pb.set_length(total_size);
-
-				// Hack to fix weird bug with `MultiProgress` where it renders an empty progress bar and leaves
-				// it there
-				if !pb_added {
-					pb.set_draw_target(indicatif::ProgressDrawTarget::stderr());
-					mpb.add(pb.clone());
-
-					pb_added = true;
-
-					if !is_tty {
-						eprintln!("Uploading {path} ({})", format_file_size(total_size)?);
-					}
-				}
-			}
-		}
-
-		// Create a reader for the slice of the file we need to read
-		file.seek(tokio::io::SeekFrom::Start(presigned_req.byte_offset as u64))
-			.await?;
-		let handle = file.take(presigned_req.content_length as u64);
-
-		// Default buffer size is optimized for memory usage. Increase buffer for perf.
-		let mut reader_stream = ReaderStream::with_capacity(handle, 1024 * 1024);
+		let command = format!(
+			"curl -X PUT -H 'Content-Length: {}' -H 'Content-Type: {}' -T '{}' '{}'",
+			presigned_req.content_length,
+			content_type
+				.clone()
+				.unwrap_or("application/octet-stream".into()),
+			tmp_file_path,
+			presigned_req.url
+		);
+		println!("command: {command}");
 
 		let start = Instant::now();
+		let output = tokio::process::Command::new("sh")
+			.arg("-c")
+			.arg(command)
+			.output()
+			.await
+			.unwrap();
+		let upload_time = start.elapsed();
+		println!("upload time {upload_time:?}");
 
-		// Process the stream with upload progress
-		let pb2 = pb.clone();
-		let async_stream = async_stream::stream! {
-			while let Some(chunk) = reader_stream.next().await {
-				if let Ok(chunk) = &chunk {
-					pb2.inc(chunk.len() as u64);
-				}
-
-				yield chunk;
-			}
-		};
-
-		let body = reqwest::Body::wrap_stream(async_stream);
-
-		// Upload file
-		let mut req = reqwest_client
-			.put(&presigned_req.url)
-			.header("content-length", presigned_req.content_length);
-		if let Some(content_type) = &content_type {
-			req = req.header("content-type", content_type.to_string());
-		}
-		let res = req.body(body).send().await?;
-		if res.status().is_success() {
-			let upload_time = start.elapsed();
-			break 'upload (upload_time, total_size);
-		} else {
-			if attempts > 4 {
-				let response_status = res.status();
-				let response_text = res.text().await?;
-				let text = format!(
-					"failed to upload file: {}\n{:?}",
-					response_status, response_text
-				);
-				bail!(&text);
-			} else {
-				attempts += 1;
-
-				let status = res.status();
-				let body_text = unwrap!(res.text().await);
-
-				pb.set_style(term::pb_style_error());
-				pb.set_message(format!(
-					"{}{}{} {path} {retry_and_body}",
-					style("[").bold().red(),
-					style(status).bold().red(),
-					style("]").bold().red(),
-					path = style(&path).red(),
-					retry_and_body =
-						style(format!("will retry (attempt #{attempts}): {body_text:?}"))
-							.dim()
-							.red(),
-				));
-
-				if !is_tty {
-					eprintln!(
-						"Error uploading {path} [{status}] (attempt #{attempts}): {body_text:?}",
-					);
-				}
-
-				tokio::time::sleep(Duration::from_secs(5)).await;
-				continue 'upload;
-			}
-		}
+		break (Duration::from_secs(0), 0);
 	};
 
 	match &main_pb {
