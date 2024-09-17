1889a72b95ac59a1f48902f1f2b7a3f1f0a719dd
NathanFlurry <git@nathanflurry.com>
2024-09-17 04:25:48 +0000

    chore(backend): add global opts to backend command wrappers (#406)

--- packages/cli/src/commands/module/format.rs
+++ packages/cli/src/commands/module/format.rs
@@ -3,9 +3,15 @@ use serde::Serialize;
 use std::process::ExitCode;
 use toolchain::backend::run_backend_command_passthrough;
 
+use crate::util::global_opts::GlobalOpts;
+
 #[derive(Parser, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Opts {
+	#[clap(flatten)]
+	#[serde(flatten)]
+	global: GlobalOpts,
+
 	#[clap(long)]
 	pub check: Option<bool>,
 }
