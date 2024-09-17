9cd323c846afdf43aa36b817ccf03a4fdfe52757
NathanFlurry <git@nathanflurry.com>
2024-09-17 04:25:42 +0000

    chore: update unity codegen to native deno (#403)

--- packages/backend/toolchain/sdk/generate.ts
+++ packages/backend/toolchain/sdk/generate.ts
@@ -5,8 +5,8 @@ import { Project } from "../project/mod.ts";
 import { projectCachePath, SDK_PATH } from "../project/project.ts";
 import { progress, success } from "../term/status.ts";
 
-import { generateTypescriptAddons } from "./typescript/mod.ts";
-import { DEFAULT_PACKAGE_NAME as UNITY_DEFAULT_PACKAGE_NAME, generateUnityAddons } from "./unity/mod.ts";
+import { generateTypescript } from "./typescript/mod.ts";
+import { generateUnity } from "./unity/mod.ts";
 import { generateGodot } from "./godot/mod.ts";
 import { SdkConfig } from "../config/project.ts";
 import { unimplemented } from "@std/assert";
@@ -18,37 +18,7 @@ export enum SdkTarget {
 }
 
 interface Generator {
-	generator: string;
-	options: Record<string, string>;
 }
-
-const GENERATORS: Record<SdkTarget, Generator> = {
-	[SdkTarget.TypeScript]: {
-		generator: "typescript-fetch",
-		options: {
-			generateAliasAsModel: "true",
-			npmName: "rivet-sdk",
-			disallowAdditionalPropertiesIfNotPresent: "false",
-			fileContentDataType: "Blob",
-			platform: "browser",
-			supportsES6: "true",
-		},
-	},
-	[SdkTarget.Unity]: {
-		generator: "csharp",
-		options: {
-			apiName: "Backend",
-			library: "unityWebRequest",
-			packageName: UNITY_DEFAULT_PACKAGE_NAME,
-			// disallowAdditionalPropertiesIfNotPresent: "false",
-			// targetFramework: "netstandard2.1",
-		},
-	},
-	[SdkTarget.Godot]: {
-		generator: "manual",
-		options: {},
-	},
-};
 
 export async function generateSdk(
 	project: Project,
@@ -68,42 +38,11 @@ export async function generateSdk(
 
 	progress("Building SDK", targetString);
 
-	const config = GENERATORS[sdk.target]!;
-	let buildOutput;
-
-	if (config.generator != "manual") {
-    unimplemented("openapi generator is unimplemented");
-		// // Run using deno when in docker
-  //   buildOutput = await new Deno.Command("docker", {
-  //     args: [
-  //       "run",
-  //       "--rm",
-  //       "-v",
-  //       `${project.cachePath}:/local`,
-  //       "openapitools/openapi-generator-cli:v7.6.0",
-  //       "generate",
-  //       "-i",
-  //       "/local/openapi.json",
-  //       "-g",
-  //       config.generator,
-  //       "-o",
-  //       `/local/sdk/${targetString}`,
-  //       "--additional-properties=" +
-  //       Object.entries(config.options).map(([key, value]) => `${key}=${value}`).join(","),
-  //     ],
-  //   }).output();
-		//
-		// if (!buildOutput.success) {
-		// 	throw new CommandError("Failed to generate OpenAPI SDK.", { commandOutput: buildOutput });
-		// }
-	}
-
 	let sdkCopyPath = sdkGenPath;
 	if (sdk.target == SdkTarget.TypeScript) {
-		await generateTypescriptAddons(project, sdkGenPath);
+		await generateTypescript(project, sdkGenPath);
 	} else if (sdk.target == SdkTarget.Unity) {
-		await generateUnityAddons(project, sdkGenPath);
-		sdkCopyPath = resolve(sdkGenPath, "src", UNITY_DEFAULT_PACKAGE_NAME);
+		await generateUnity(project, sdkGenPath);
 	} else if (sdk.target == SdkTarget.Godot) {
 		await generateGodot(project, sdkGenPath);
 	} else {
