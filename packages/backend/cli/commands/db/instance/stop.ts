06b5a5813874b408d471bb0cbcc8456cc411114d
Nathan Flurry <git@nathanflurry.com>
2024-09-17 00:49:03 -0700

    fix(backend): fix incorrect status/stop commands

--- packages/backend/cli/commands/db/instance/stop.ts
+++ packages/backend/cli/commands/db/instance/stop.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 import { globalOptsSchema, initProject } from "../../../common.ts";
 import { getDefaultPostgresManager } from "../../../../toolchain/postgres/mod.ts";
-import { Status, status } from "../../../../toolchain/postgres/manager.ts";
-import { info, warn } from "../../../../toolchain/term/status.ts";
-import { UnreachableError } from "../../../../toolchain/error/mod.ts";
+import { stop } from "../../../../toolchain/postgres/manager.ts";
+import { success, warn } from "../../../../toolchain/term/status.ts";
 
 export const optsSchema = globalOptsSchema;
 
@@ -13,24 +12,8 @@ export async function execute(opts: Opts) {
 	const project = await initProject(opts);
 	const manager = await getDefaultPostgresManager(project);
 	if (manager) {
-		let statusText: string;
-		const currentStatus = await status(manager);
-		if (currentStatus === Status.NotInstalled) {
-			statusText = "Not installed";
-		} else if (currentStatus === Status.Installed) {
-			statusText = "Installed";
-		} else if (currentStatus === Status.Initialized) {
-			statusText = "Initialized";
-		} else if (currentStatus === Status.DefaultDatabaseNotCreated) {
-			statusText = "Default database not created";
-		} else if (currentStatus === Status.Stopped) {
-			statusText = "Stopped";
-		} else if (currentStatus === Status.Started) {
-			statusText = "Started";
-		} else {
-			throw new UnreachableError(currentStatus);
-		}
-		info("Status", statusText);
+		await stop(manager);
+		success("Postgres instance stopped");
 	} else {
 		warn("Postgres is disabled");
 	}
