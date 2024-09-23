c0b1ab49330e2f914870b586f034d1e910597d9b
Nathan Flurry <developer@nathanflurry.com>
2024-09-23 01:24:24 -0700

    fix(backend): remove use of locks on configManifestPath, configOutputManifestPath, and configShow

--- packages/backend/cli/commands/config/manifest_path.ts
+++ packages/backend/cli/commands/config/manifest_path.ts
@@ -1,12 +1,17 @@
 import { z } from "zod";
-import { globalOptsSchema, initProject } from "../../common.ts";
-import { PROJECT_MANIFEST_PATH, projectCachePath } from "../../../toolchain/project/mod.ts";
+import { globalOptsSchema } from "../../common.ts";
+import { computeProjectCachePath, loadProjectConfigPath, PROJECT_MANIFEST_PATH } from "../../../toolchain/project/mod.ts";
+import { dirname, resolve } from "@std/path";
 
 export const optsSchema = z.object({}).merge(globalOptsSchema);
 
 type Opts = z.infer<typeof optsSchema>;
 
 export async function execute(opts: Opts) {
-	const project = await initProject(opts);
-	console.log(projectCachePath(project, PROJECT_MANIFEST_PATH));
+  // Don't load project since that requires acquiring a lock on the project
+
+	const projectRoot = dirname(loadProjectConfigPath(opts));
+	const cachePath = await computeProjectCachePath(projectRoot);
+  const manifestPath = resolve(cachePath, PROJECT_MANIFEST_PATH)
+	console.log(manifestPath);
 }
--- packages/backend/cli/commands/config/output_manifest_path.ts
+++ packages/backend/cli/commands/config/output_manifest_path.ts
@@ -1,13 +1,18 @@
 import { z } from "zod";
 import { globalOptsSchema, initProject } from "../../common.ts";
 import { OUTPUT_MANIFEST_PATH } from "../../../toolchain/project/mod.ts";
-import { projectCachePath } from "../../../toolchain/project/project.ts";
+import { computeProjectCachePath, loadProjectConfigPath, projectCachePath } from "../../../toolchain/project/project.ts";
+import { dirname, resolve } from "@std/path";
 
 export const optsSchema = z.object({}).merge(globalOptsSchema);
 
 type Opts = z.infer<typeof optsSchema>;
 
 export async function execute(opts: Opts) {
-	const project = await initProject(opts);
-	console.log(projectCachePath(project, OUTPUT_MANIFEST_PATH));
+  // Don't load project since that requires acquiring a lock on the project
+
+	const projectRoot = dirname(loadProjectConfigPath(opts));
+	const cachePath = await computeProjectCachePath(projectRoot);
+  const manifestPath = resolve(cachePath, OUTPUT_MANIFEST_PATH)
+	console.log(manifestPath);
 }
