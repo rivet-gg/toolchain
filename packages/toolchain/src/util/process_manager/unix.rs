dc617f928df8dfa1fc0678910b4e54cd18628fcd
Nathan Flurry <developer@nathanflurry.com>
2024-09-30 05:47:37 -0700

    fix(toolchain): remove use of libproc on macos in favor of pkill

--- packages/toolchain/src/util/process_manager/unix.rs
+++ packages/toolchain/src/util/process_manager/unix.rs
@@ -11,28 +11,64 @@ pub fn send_terminate_signal(pid: u32) {
 	}
 }
 
-/// Kill process tree using libproc on macOS. This is not portable to Linux distros.
+/// Kill process tree using pkill on macOS. This is not portable to Linux distros.
 #[cfg(target_os = "macos")]
-pub fn kill_process_tree(pid_raw: u32) {
-	use libproc::processes::{pids_by_type, ProcFilter};
+pub fn kill_process_tree(pid: u32) {
+	use std::process::Command;
 
-	let pid = Pid::from_raw(pid_raw as i32);
-
-	match pids_by_type(ProcFilter::ByParentProcess { ppid: pid_raw }) {
-		Result::Ok(pids) => {
-			for &child_pid in &pids {
-				kill_process_tree(child_pid);
+	// Kill the children
+	match Command::new("pkill")
+		.args(&["-9", "-P", &pid.to_string()])
+		.status()
+	{
+		Result::Ok(status) => {
+			if !status.success() {
+				eprintln!("pkill failed to kill children: status {status}");
 			}
 		}
-		Err(e) => {
-			eprintln!("Failed to list child processes for {}: {}", pid_raw, e);
+		Err(err) => {
+			eprintln!("pkill failed to kill children: {err}");
+		}
+	}
+
+	// Kill the parent
+	match Command::new("kill")
+		.args(&["-9", &pid.to_string()])
+		.status()
+	{
+		Result::Ok(status) => {
+			if !status.success() {
+				eprintln!("kill failed to kill parent: status {status}");
+			}
+		}
+		Err(err) => {
+			eprintln!("kill failed to kill parent: {err:?}");
 		}
 	};
-
-	if let Err(e) = kill(pid, Signal::SIGKILL) {
-		eprintln!("Failed to kill process {}: {}", pid_raw, e);
-	}
 }
+
+// TODO: Figure out how to make libproc work with osxcross
+// #[cfg(target_os = "macos")]
+// pub fn kill_process_tree(pid_raw: u32) {
+// 	use libproc::processes::{pids_by_type, ProcFilter};
+//
+// 	let pid = Pid::from_raw(pid_raw as i32);
+//
+// 	match pids_by_type(ProcFilter::ByParentProcess { ppid: pid_raw }) {
+// 		Result::Ok(pids) => {
+// 			for &child_pid in &pids {
+// 				kill_process_tree(child_pid);
+// 			}
+// 		}
+// 		Err(e) => {
+// 			eprintln!("Failed to list child processes for {}: {}", pid_raw, e);
+// 		}
+// 	};
+//
+// 	if let Err(e) = kill(pid, Signal::SIGKILL) {
+// 		eprintln!("Failed to kill process {}: {}", pid_raw, e);
+// 	}
+// }
 
 /// Kill process tree using `/proc/` on Linux.
 #[cfg(target_os = "linux")]
