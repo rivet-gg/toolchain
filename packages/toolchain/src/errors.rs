d7a6508dbfc76cafc8fe8902e88a59032ff08f98
NathanFlurry <git@nathanflurry.com>
2024-12-01 17:25:29 +0000

    chore: add sentry integration (#592)
    Fixes RVT-4143

--- packages/toolchain/src/errors.rs
+++ packages/toolchain/src/errors.rs
@@ -18,6 +18,25 @@ impl std::fmt::Display for GracefulExit {
 
 impl std::error::Error for GracefulExit {}
 
+/// This error type will exit without printing anything.
+///
+/// This indicates the program was exited with a Ctrl-C event.
+pub struct CtrlC;
+
+impl std::fmt::Debug for CtrlC {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.debug_struct("CtrlC").finish()
+	}
+}
+
+impl std::fmt::Display for CtrlC {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "CtrlC occurred")
+	}
+}
+
+impl std::error::Error for CtrlC {}
+
 /// This error type will exit with a message, but will not report the error to Rivet.
 ///
 /// This should be used for errors where the user input is incorrect.
